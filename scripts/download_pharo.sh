2d88c7c0455b422be32caaa18e05baec32a2d3c0
Torsten Bergmann <astares@gmx.de>
2022-12-13 08:24:17 +0100

    documen Pharo download script

--- scripts/download_pharo.sh
+++ scripts/download_pharo.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 #############################################################
+# 
+# This script downloads Pharo and loads the application
+#
+#############################################################
 # (c) 2022, Dipl.-Inf. T. Bergmann - astares.com
 #############################################################
 
-#
+# Check for the base directory to be given as argument
 if [[ $# -eq 0 ]] ; then
     echo 'Provide the absolute file path as an argument'
     exit 0
 fi
 
+# Build directory
 BUILD_PATH="$1/build"
 
 # If there is an old build directory then remove it
@@ -18,24 +23,12 @@ if [ -d build ]; then
     rm -Rf "$BUILD_PATH" 
 fi
 
+# Create and switch to build directory
 mkdir "$BUILD_PATH"
 cd "$BUILD_PATH"
 
-# Download
+# Download Pharo using ZeroConf (http://get.pharo.org)
 wget -O- https://get.pharo.org/64/100+vm | bash
 
-./pharo-ui ./Pharo.image eval "
-|creator repo|
-Author uniqueInstance fullName: 'TorstenBergmann'.
-
-creator := IceRepositoryCreator new.
-repo := creator repository.
-repo 
-    name: 'Themis';
-    location: '$1' asFileReference.
-
-repo register.
-
-IceTipRepositoriesBrowser new open
-
-"
+# Start Pharo with the load script
+./pharo-ui ./Pharo.image ../scripts/load.st
